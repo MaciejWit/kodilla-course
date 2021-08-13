de0c4fdbd80cbedfc3b60097b7953ee6a38dc53e
MaciejWit <maciej.wit2@gmail.com>
2021-08-13 20:47:38 +0200

    Fixed Structure

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Aplication.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing.calculator;
+
+public class Aplication {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = calculator.addAToB(5, 5);
+        System.out.println(result);
+    }
+}
